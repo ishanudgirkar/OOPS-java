a9ab64c09f839463fc93523819e9ed5ec9f493fc
Ishan <udgirkarishan@gmail.com>
2023-03-10 19:33:56 +0530

    class code

---
+++ Class/Demo.java
@@ -0,0 +1,10 @@
+class Demo {
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        System.out.println(d.name);
+        System.out.println(d.age);
+    }
+}
